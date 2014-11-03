Added the ipython notebood and squashed a bug in the complementary pair computation.

Biological_trap.py
@@ -5,9 +5,9 @@ from csv import reader
 from os import path
 from matplotlib import pyplot as plt
 from math import sqrt
-from Support import pretty_gradual_plot, get_resistant_susceptible
+from Support import pretty_gradual_plot, get_resistant_susceptible, pretty_get_resistant_susceptible
 import matplotlib as mlb
-from itertools import permutations, product
+from itertools import product
 from Linalg_routines import hierchical_clustering, show_matrix_with_names
 from scipy.stats import gaussian_kde
 from matplotlib.pyplot import Rectangle
@@ -96,7 +96,8 @@ with open(path.join(pth, fle)) as src:
         background[cell_no, drug_no, :] = np.array([row[i] for i in [4, 5, 36, 37]])
         concentrations[cell_no, drug_no, :] = np.array([0]+row[39:48])
 
-# print storage
+
+# print storage.shape
 # print background
 # print concentrations
 # print np.std(remove_nan(background))
@@ -120,6 +121,10 @@ foldlist = []
 for index, drug in sorted(drug_idx_rv.items()):
     drug_action = storage[:, drug_idx[drug], :, :]
     foldlist.append(get_resistant_susceptible(drug_action, drug, blank_line=sensible_max))
+    # if 'Tykerb' in drug:
+    #     pretty_gradual_plot(storage[:, index, :, :], concentrations[:, index, :], cell_idx_rv, drug, blank_line=sensible_max)
+    #     pretty_get_resistant_susceptible(drug_action, cell_idx_rv, drug,
+    #             blank_line=sensible_max, concentrations=concentrations[:, drug_idx[drug], :])
 flds = np.array(foldlist)
 
 
@@ -195,7 +200,7 @@ def plot_action_map(fields, Drug1=None, Drug2=None):
         plt.yticks(range(0, len(drug_idx)), [drug for i, drug in sorted(drug_idx_rv.items())], rotation='horizontal', )
         plt.xticks(range(0, len(cell_idx)), [cell for i, cell in sorted(cell_idx_rv.items())], rotation='vertical')
         plt.legend((red, light_red, grey, light_blue, blue),
-                   ('Resistant', 'Partially Resistant', 'No specific phenotype', 'Susceptible', 'Killed'),
+                   ('Resistant', 'Partially Resistant', 'Weakly resistant', 'Susceptible', 'Killed'),
                    bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand", borderaxespad=0.,)
         plt.show()
 
@@ -281,7 +286,9 @@ def round_show(matrix):
     # plot_action_map(flds, int(coords[0]), int(coords[1]))
     return index
 
-# plot_action_map(flds)
+
+
+plot_action_map(flds)
 # plot_action_map(flds, 'Rapamycin', 'Fascaplysin')
 
 # per_flds = random_permute(flds, 1)

Support.py
@@ -81,7 +81,7 @@ def pretty_gradual_plot(data, concentrations, strain_name_map, drug_name, blank_
     plt.show()
 
 
-def pretty_get_resistant_susceptible(data, strain_name_map, drug_name, blank_line=200):
+def pretty_get_resistant_susceptible(data, strain_name_map, drug_name, blank_line=200, concentrations=[]):
 
     def inner_scatter_plot(mean, std, names, selector=None, relative=False, limiter=4):
         if selector is not None:
@@ -112,30 +112,33 @@ def pretty_get_resistant_susceptible(data, strain_name_map, drug_name, blank_lin
     rel_mean, rel_std = (mean/refmean, np.sqrt(np.power(refstd, 2)+np.power(std, 2))/mean)
 
     # From here, we are processing just the best survivors v.s. worst survivors
-    dead = mean-std<blank_line
+    dead = mean - std < blank_line
 
-    # first, filter out all those that cannot be distinguished from background
-    upper = np.percentile(rel_mean, 95, axis = 0).reshape(1, rel_mean.shape[1])
-    lower = np.percentile(rel_mean, 5, axis = 0).reshape(1, rel_mean.shape[1])
+    rel_mean[dead] = 0
+    upper = np.percentile(rel_mean, 95, axis=0).reshape(1, rel_mean.shape[1])
+    lower = np.percentile(rel_mean, 5, axis=0).reshape(1, rel_mean.shape[1])
     upper[0, :-2] = 100
     lower[0, :-2] = 0
 
     destroyed = np.any(dead, axis=1).nonzero()[0]
-    resistant = list(set(np.any(rel_mean>upper, axis=1).nonzero()[0])-set(destroyed))
-    susceptible = list(set(np.any(rel_mean<lower, axis=1).nonzero()[0])-set(destroyed))
+
+    resistant = list(set(np.any(rel_mean>upper, axis=1).nonzero()[0]) - set(destroyed))
+    susceptible = list(set(np.any(rel_mean<lower, axis=1).nonzero()[0]) - set(destroyed))
 
     ret_array = np.empty(data.shape[0])
-    ret_array.fill(np.NaN) # we might try to do somethign to distinguish the "no effect" from "no data"
+    ret_array.fill(np.NaN)
+    ret_array[reverse_filter] = 0
     ret_array[reverse_filter[resistant]] = 1
-    ret_array[reverse_filter[susceptible]] = 0
+    ret_array[reverse_filter[susceptible]] = -0.5
     ret_array[reverse_filter[destroyed]] = -1
 
-    if len(resistant) < 1:
+    print len(reverse_filter), len(resistant), len(susceptible), len(destroyed)
+
+    if len(resistant)<1:
         int_ret = pretty_get_resistant_susceptible(data[:,:-1,:], strain_name_map, drug_name, blank_line=blank_line)
-        int_ret[int_ret>0] = int_ret[int_ret>0] * 0.5
+        int_ret[int_ret>0] = int_ret[int_ret>0]*0.5
         flt = int_ret > 0
         ret_array[flt] = int_ret[flt]
-        return ret_array
 
     inner_scatter_plot(mean, std, names, resistant, False, 1)
     inner_scatter_plot(rel_mean, rel_std, names, resistant, True, 1)
@@ -152,6 +155,7 @@ def pretty_get_resistant_susceptible(data, strain_name_map, drug_name, blank_lin
 
     return ret_array
 
+
 def get_resistant_susceptible(data, drug_name, blank_line=200):
 
     filter = np.all(np.logical_not(np.isnan(data)), axis=(1, 2))
@@ -167,6 +171,7 @@ def get_resistant_susceptible(data, drug_name, blank_line=200):
     dead = mean-std<blank_line
 
     # first, filter out all those that cannot be distinguished from background
+    rel_mean[dead] = 0
     upper = np.percentile(rel_mean, 95, axis = 0).reshape(1, rel_mean.shape[1])
     lower = np.percentile(rel_mean, 5, axis = 0).reshape(1, rel_mean.shape[1])
     upper[0, :-2] = 100