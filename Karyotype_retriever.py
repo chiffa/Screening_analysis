46f3c6736654390b2c0ae3912d1cd76afff8cbcc
chiffa <andrei.chiffa136@gmail.com>
2014-11-20 21:21:45 -0600

    - functionality-preserving refactoring  - Added the iterative HMM region recognition  - Randomized percentile threshold (70-80%)

--- Karyotype_retriever.py
+++ Karyotype_retriever.py
@@ -12,6 +12,8 @@ import scipy.cluster.hierarchy as sch
 from scipy.spatial.distance import pdist
 from scipy.ndimage.filters import gaussian_filter1d as smooth_signal
 import warnings
+from itertools import izip
+import copy
 
 # intra-chromosome v.s. interchromosome variance?
 # normalized within 50% lowest of the variance within a single chromosome?
@@ -39,7 +41,6 @@ locuses = np.array(locuses)
 chr_arr = locuses[:, 0]
 broken_table = []
 chroms = range(int(np.min(chr_arr)), int(np.max(chr_arr))+1)
-print 'chroms:', chroms
 for i in chroms:
     broken_table.append(chr_arr == i)
 
@@ -55,14 +56,12 @@ initial_dist = np.array([[0.33, 0.34, 0.33]])
 emission_probs  = np.ones((3, 3)) * 0.1
 np.fill_diagonal(emission_probs, 0.8)
 parsing_hmm = hmm.HMM(transition_probs, emission_probs)
-parsing_hmm2 = hmm.HMM(transition_probs, emission_probs)
 
 warnings.catch_warnings()
 warnings.simplefilter("ignore")
 
-def t_test_matrix(lane):
+def t_test_matrix(current_lane):
 
-    current_lane = locuses[:, lane]
     dst = broken_table.shape[0]
     p_vals = np.empty((dst, dst))
     p_vals.fill(np.NaN)
@@ -78,7 +77,105 @@ def rolling_window(a, window):
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
 
-def compute_karyotype(lane, plotting=False, threshold = 0.33):
+def pull_breakpoints(contingency_list):
+    no_nans_parsed = rm_nans(contingency_list)
+    contingency = np.lib.pad(no_nans_parsed[:-1] == no_nans_parsed[1:], (1, 0), 'constant', constant_values=(True, True))
+    nans_contingency = np.zeros(contingency_list.shape).astype(np.bool)
+    nans_contingency[np.logical_not(np.isnan(contingency_list))] = contingency
+    breakpoints = np.nonzero(np.logical_not(nans_contingency))[0].tolist()
+    return breakpoints
+
+
+def show_breakpoints(breakpoints):
+    for point in breakpoints:
+        plt.axvline(x=point, color='b')
+
+
+######################################
+# TODO: attention: currently instable
+def t_statistic_sorter(current_lane):
+
+    t_mat = t_test_matrix(current_lane)
+    t_mat[np.isnan(t_mat)] = 0
+    t_mat = t_mat + t_mat.T
+    np.fill_diagonal(t_mat, 1)
+    ct_mat = t_mat.copy()
+    ct_mat[t_mat < 0.01] = 0.01
+    ct_mat = 1 - ct_mat
+
+    Y = sch.linkage(ct_mat, method='centroid')
+    clust_alloc = sch.fcluster(Y, 0.95, criterion='distance')
+
+    averages = []
+    for i in range(0, 24):
+        averages.append(np.median(rm_nans(current_lane[broken_table[i]])))  # TODO: correct here to remove the broken table, insert breakpoint selector
+
+    accumulator = [[] for _ in range(0, max(clust_alloc)+1)]
+    for loc, item in enumerate(averages):
+        accumulator[clust_alloc[loc]].append(item)
+    accumulator = np.array([ np.average(np.array(_list)) for _list in accumulator][1:])
+
+    splitting_matrix = np.repeat(accumulator.reshape((1, accumulator.shape[0])),
+                                    accumulator.shape[0], axis = 0)
+    splitting_matrix = np.abs(splitting_matrix - splitting_matrix.T)
+
+    plt.imshow(splitting_matrix, interpolation='nearest', cmap='coolwarm')
+    plt.show()
+
+    Y_2 = sch.linkage(splitting_matrix, method='centroid')
+    clust_alloc_2 = sch.fcluster(Y_2, 3, criterion='maxclust')
+
+    accumulator_2 = [[] for _ in range(0, max(clust_alloc_2)+1)]
+    for loc, item in enumerate(accumulator):
+        accumulator_2[clust_alloc_2[loc]].append(item)
+    accumulator_2 = np.array([ np.average(np.array(_list)) for _list in accumulator_2][1:])
+
+    sorter_l = np.argsort(accumulator_2)
+    sorter = dict((pos, i) for i, pos in enumerate(sorter_l))
+
+    re_chromosome_pad = np.repeat(locuses[:, 0].reshape((1, locuses.shape[0])),100, axis=0)
+    re_classification_tag = np.zeros(re_chromosome_pad.shape)
+
+    for i in range(0, len(clust_alloc)):
+        re_classification_tag[re_chromosome_pad == i+1] = averages[i]
+        # re_classification_tag[re_chromosome_pad == i+1] = accumulator[ clust_alloc[i]-1]
+        # re_classification_tag[re_chromosome_pad == i+1] = sorter[clust_alloc_2[clust_alloc[i]-1]-1]
+
+    #################################################################################
+    ax1 = plt.subplot(211)
+    plt.imshow(chromosome_tag, interpolation='nearest', cmap='spectral')
+    plt.imshow(re_classification_tag, interpolation='nearest', cmap='coolwarm')
+    plt.setp(ax1.get_xticklabels(), fontsize=6)
+    ax2 = plt.subplot(212, sharex=ax1)
+    plt.plot(current_lane-np.average(rm_nans(current_lane)), 'k.')
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.show()
+    ################################################################################
+
+    return re_classification_tag
+
+
+##############################################################################################
+##############################################################################################
+# TODO: idea: keep the HMM model for the regression, but determine bounds by normalizing
+#   On the per-chromosome basis: compute the mean and std
+#   Eliminate the chromosomes with highest std (possibly contain inner amplifications)
+#   determine the bounds of the HMM model.
+#   Run the HMM rounds iteratively,
+#       On each iteration, remove losses/gains from the previous model.
+#       End up with discretized levels.
+#   Try to guess most likely integer levels of chromosomes/locus gains.
+#
+#   Now, how can we turn the matrix into a recursive function?
+#
+#   - We might also need to classify separatly large amplifications(arm-level) from short ones
+#       (a couple of tags)
+#
+##############################################################################################
+##############################################################################################
+
+
+def compute_karyotype(current_lane, plotting=False):
 
     def support_function(x, y):
         if x == 0 and y == 0:
@@ -93,41 +190,30 @@ def compute_karyotype(lane, plotting=False, threshold = 0.33):
     def plot_classification():
 
         classification_tag = np.repeat(parsed.reshape((1, parsed.shape[0])), 100, axis=0)
-        classification_tag2 = np.repeat(parsed2.reshape((1, parsed.shape[0])), 100, axis=0)
         ax1 = plt.subplot(311)
         plt.imshow(chromosome_tag, interpolation='nearest', cmap='spectral')
-        plt.imshow(classification_tag, interpolation='nearest', cmap='coolwarm')
+        plt.imshow(classification_tag, interpolation='nearest', cmap='coolwarm', vmin=0, vmax=2)
         plt.setp(ax1.get_xticklabels(), fontsize=6)
         ax2 = plt.subplot(312, sharex=ax1)
-        plt.plot(current_lane, 'k.', label = '%s %s %s'%(lane,
-                                                         '{0:.2f}'.format(np.mean(rm_nans(current_lane))),
-                                                         '{0:.2f}'.format(np.std(rm_nans(current_lane)))))
+        plt.plot(current_lane, 'k.')
         plt.plot(gauss_convolve, 'r', lw=2)
         plt.plot(gauss_convolve+rolling_std, 'g', lw=1)
         plt.plot(gauss_convolve-rolling_std, 'g', lw=1)
-        plt.axhline(y=threshold, color='b')
-        plt.axhline(y=-threshold, color='b')
-        plt.axhline(y=prct_threshold, color='c')
-        plt.axhline(y=-prct_threshold, color='c')
-        plt.legend(prop={'size':10})
+        plt.plot(segment_averages, 'b', lw=2)
+        plt.axhline(y=threshold, color='c')
+        plt.axhline(y=-threshold, color='c')
+        # for point in breakpoints:
+        #     plt.axvline(x=point, color='b')
         plt.setp(ax2.get_xticklabels(), visible=False)
         ax3 = plt.subplot(313, sharex=ax1)
-        # plt.plot(total_corr, 'k.')
-        # plt.plot(gauss_convolve, 'k.')
-        # plt.plot(rolling_std, 'g')
-        plt.imshow(chromosome_tag, interpolation='nearest', cmap='spectral')
-        plt.imshow(classification_tag2, interpolation='nearest', cmap='coolwarm')
+        plt.plot(current_lane-segment_averages, 'k.')
+        # # plt.plot(total_corr, 'k.')
+        # # plt.plot(gauss_convolve, 'k.')
+        # # plt.plot(rolling_std, 'g')
         # plt.setp(ax3.get_xticklabels(), visible=False)
         plt.show()
 
-    current_lane = locuses[:, lane]
     current_lane = current_lane - np.mean(rm_nans(current_lane))
-    current_lane = current_lane / np.std(rm_nans(current_lane))*0.3
-    binarized = (current_lane > threshold).astype(np.int16) - (current_lane < -threshold) + 1
-    parsed = np.array(hmm.viterbi(parsing_hmm, initial_dist, binarized))
-
-    partial_corr = np.correlate(rm_nans(current_lane), rm_nans(current_lane), mode='full')
-    total_corr = partial_corr[partial_corr.size/2:]
 
     gauss_convolve = np.empty(current_lane.shape)
     gauss_convolve.fill(np.NaN)
@@ -142,92 +228,83 @@ def compute_karyotype(lane, plotting=False, threshold = 0.33):
                                                                                  'constant',
                                                                                  constant_values=(np.NaN, np.NaN))
 
-    # nns_rolling_std = rm_nans(rolling_std)
-    # print(np.percentile(nns_rolling_std, 50),
-    #       np.percentile(nns_rolling_std, 75),
-    #       np.percentile(nns_rolling_std, 80),
-    #       np.percentile(nns_rolling_std, 90),
-    #       np.percentile(nns_rolling_std, 95))
+    corrfact = np.random.randint(-5, 5)
+    threshold = np.percentile(rm_nans(rolling_std), 75+corrfact)
+    binarized = (current_lane > threshold).astype(np.int16) - (current_lane < -threshold) + 1
+    parsed = np.array(hmm.viterbi(parsing_hmm, initial_dist, binarized))
 
-    prct_threshold = np.percentile(rm_nans(rolling_std), 75)
-    print prct_threshold
-    binarized2 = (current_lane > prct_threshold).astype(np.int16) - (current_lane < -prct_threshold) + 1
-    parsed2 = np.array(hmm.viterbi(parsing_hmm2, initial_dist, binarized2))
+    breakpoints = pull_breakpoints(parsed)
 
-    # print broken_table.shape[0]
-    # for i in range(0, broken_table.shape[0]):
-    #     current_chromosome = rm_nans(current_lane[broken_table[i, :]])
-    #     print i+1, np.mean(current_chromosome), np.std(current_chromosome)
-    #     print '\t', np.median(current_chromosome), (np.percentile(current_chromosome, 85)-np.percentile(current_chromosome, 15))/2.0
-    #
+    segment_averages = np.empty(current_lane.shape)
+    subsets = np.split(current_lane, breakpoints)
 
+    pre_brp = 0
+    breakpoints.append(current_lane.shape[0])
 
-    t_mat = t_test_matrix(lane)
-    t_mat[np.isnan(t_mat)] = 0
-    t_mat = t_mat + t_mat.T
-    np.fill_diagonal(t_mat, 1)
-    ct_mat = t_mat.copy()
-    ct_mat[t_mat < 0.01] = 0.01
-    ct_mat = 1 - ct_mat
+    for subset, brp in izip(subsets, breakpoints):
+        av = np.average(rm_nans(subset))
+        segment_averages[pre_brp : brp] = av
+        pre_brp = brp
 
-    # TODO: idea: keep the HMM model for the regression, but determine bounds by normalizing
-    #   On the per-chromosome basis: compute the mean and std
-    #   Eliminate the chromosomes with highest std (possibly contain inner amplifications)
-    #   determine the bounds of the HMM model.
-    #   Run the HMM rounds iteratively,
-    #       On each iteration, remove losses/gains from the previous model.
-    #       End up with discretized levels.
-    #   Try to gess most likely integer levels of chromosomes/locus gains.
-
-    Y = sch.linkage(ct_mat, method='centroid')
-    clust_alloc = sch.fcluster(Y, 0.95, criterion='distance')
-
-    averages = []
-    for i in range(0, 24):
-        averages.append(np.median(rm_nans(current_lane[broken_table[i]])))
-
-    accumulator = [[] for _ in range(0, max(clust_alloc)+1)]
-    for loc, item in enumerate(averages):
-        accumulator[clust_alloc[loc]].append(item)
-    accumulator = np.array([ np.average(np.array(_list)) for _list in accumulator][1:])
-
-    splitting_matrix = np.repeat(accumulator.reshape((1, accumulator.shape[0])),
-                                    accumulator.shape[0], axis = 0)
-    splitting_matrix = np.abs(splitting_matrix - splitting_matrix.T)
-
-    # plt.imshow(splitting_matrix, interpolation='nearest', cmap='coolwarm')
-    # plt.show()
-
-    Y_2 = sch.linkage(splitting_matrix, method='centroid')
-    clust_alloc_2 = sch.fcluster(Y_2, 3, criterion='maxclust')
-
-    accumulator_2 = [[] for _ in range(0, max(clust_alloc_2)+1)]
-    for loc, item in enumerate(accumulator):
-        accumulator_2[clust_alloc_2[loc]].append(item)
-    accumulator_2 = np.array([ np.average(np.array(_list)) for _list in accumulator_2][1:])
-
-    sorter_l = np.argsort(accumulator_2)
-    sorter = dict((pos, i) for i, pos in enumerate(sorter_l))
-
-    re_chromosome_pad = np.repeat(locuses[:, 0].reshape((1, locuses.shape[0])), 100, axis=0)
-    re_classification_tag = np.zeros(re_chromosome_pad.shape)
-
-    for i in range(0, len(clust_alloc)):
-        # re_classification_tag[re_chromosome_pad == i+1] = averages[i]
-        # re_classification_tag[re_chromosome_pad == i+1] = accumulator[ clust_alloc[i]-1]
-        re_classification_tag[re_chromosome_pad == i+1] = sorter[clust_alloc_2[clust_alloc[i]-1]-1]
-
+    # TODO: Needs to be moved to the outer scope
+    #############################################################
     collector = []
     for i in chroms:
         lw = np.percentile(parsed[broken_table[i-1, :]]-1, 25)
         hr = np.percentile(parsed[broken_table[i-1, :]]-1, 75)
         collector.append(support_function(lw, hr))
+    ############################################################
 
     if plotting:
         plot_classification()
 
-    collector = np.array(collector)
-    return collector
+    return current_lane-segment_averages, segment_averages, parsed, np.array(collector)
+
+
+def compute_recursive_karyotype(lane, plotting=False):
+
+    current_lane = locuses[:, lane]
+
+    classification_tag2 = t_statistic_sorter(current_lane)
+
+    retlist = compute_karyotype(current_lane, plotting=plotting)
+
+    amplicons = retlist[1]
+    ampli_levels = retlist[2]-1
+    re_retlist = copy.deepcopy(retlist)
+    for i in range(0, 6):
+        re_retlist = compute_karyotype(re_retlist[0], plotting=plotting)
+        if np.max(re_retlist[2])-np.min(re_retlist[2]) < 1:
+            break
+        else:
+            amplicons += re_retlist[1]
+            ampli_levels += re_retlist[2] - 1
+
+
+    # TODO: insert the t-test balancer of segments in order to iteratively remove "short"
+    #       segments and insert into longer ones.
+
+    classification_tag = np.repeat(ampli_levels.reshape((1, ampli_levels.shape[0])), 100, axis=0)
+    ax1 = plt.subplot(211)
+    plt.imshow(chromosome_tag, interpolation='nearest', cmap='spectral')
+    plt.imshow(classification_tag, interpolation='nearest', cmap='coolwarm')
+    plt.imshow(classification_tag2, interpolation='nearest', cmap='coolwarm')
+    plt.setp(ax1.get_xticklabels(), fontsize=6)
+    ax2 = plt.subplot(212, sharex=ax1)
+    plt.plot(locuses[:, lane]-np.average(rm_nans(locuses[:, lane])), 'k.')
+    plt.plot(amplicons, 'r', lw=2)
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.show()
+
+    # #############################################################
+    # collector = []
+    # for i in chroms:
+    # lw = np.percentile(parsed[broken_table[i-1, :]]-1, 25)
+    # hr = np.percentile(parsed[broken_table[i-1, :]]-1, 75)
+    # collector.append(support_function(lw, hr))
+    # ############################################################
+
+    return retlist
 
 
 def compute_all_karyotypes():
@@ -238,7 +315,8 @@ def compute_all_karyotypes():
 
     chromlist = []
     for i in range(1, locuses.shape[1]):
-        chromlist.append(compute_karyotype(i, plotting=True, threshold=0.35))
+        print i
+        chromlist.append(compute_recursive_karyotype(i, plotting=True)[-1])
     chromlist = np.array(chromlist).astype(np.float64)
     return chromlist, header[1:locuses.shape[1]]
 
